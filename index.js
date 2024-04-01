412a47c1e356388b385d5b0785d328bb246e301f
Cauan Magno <cauan.magno@gmail.com>
2024-04-01 10:24:33 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html> 
+<head>
+    <meta charset="UTF-8">
+    <title>Javascript Hello Wod</title>
+
+    <script>
+function main() {
+    alert("Hello Word");
+}
+</script>
+</head>
+<body>
+
+</body>
+</html>
